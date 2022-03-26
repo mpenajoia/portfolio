3821a30ea7186ff160dd2096d85e41db0f6c3eb4
mpenajoia <penajoia.marco@gmail.com>
2022-03-25 17:22:07 -0700

    intial commit, tailwind install

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+  return (
+    <div>
+      <nav>NAVBAR</nav>
+      <div>
+        Hero
+      </div>
+      <div>
+        About
+      </div>
+      <div>
+        Projects
+      </div>
+      <div>
+        Contact
+      </div>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
