35d2f11333f28bc5fc51da5556f24b0574ab9819
mpenajoia <penajoia.marco@gmail.com>
2022-03-28 21:15:17 -0700

    Contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='flex justify-center my-10'>
+        <div className='flex flex-col w-2/5'>
+            <h2 className='text-4xl font-bold my-2'>Get in touch</h2>
+            <form className='flex flex-col gap-y-3'>
+                <input required type="text" name="name" placeholder='Your Name *' className='rounded px-3 py-2 focus:outline-none bg-zinc-900 placeholder:text-zinc-500 '/>
+                <input required type="text" name="email" placeholder='Your email *' className='rounded px-3 py-2 focus:outline-none bg-zinc-900  placeholder:text-zinc-500 '/>
+                <input required type="text" name="subject" placeholder='Subject *' className='rounded px-3 py-2 focus:outline-none bg-zinc-900  placeholder:text-zinc-500 '/>
+                <textarea required type="text" rows="5" name="message" placeholder='Message *' className='rounded px-3 py-2 focus:outline-none bg-zinc-900  placeholder:text-zinc-500 '/>
+            </form>
+        </div>
+    </div>
   )
 }
 
