9093ad396d056d0930c537e3b26b668453cd1921
mpenajoia <penajoia.marco@gmail.com>
2022-03-29 17:19:53 -0700

    project images

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,10 +4,12 @@ import { ObjectContext } from '../context/ObjectContext';
 
 
 function ProjectCard(project, key) {
+  console.log(project.image)
   return (
     <div key={key} className='w-1/4 p-3 bg-gradient-to-t from-yellow-500 to-yellow-400 hover:to-yellow-500 rounded-md flex flex-wrap text-black translate-y-0 hover:-translate-y-4 ease-in-out duration-500'>
       <p>{project.title}</p>
       <p>{project.link}</p>
+      <img src={project.image} alt={project.title} />
     </div>
   )
 }
