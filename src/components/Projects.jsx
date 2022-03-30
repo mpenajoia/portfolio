437040deaee773e0935ce789a10e9abeee113078
mpenajoia <penajoia.marco@gmail.com>
2022-03-29 21:24:04 -0700

    opacity project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,9 +11,11 @@ function ProjectCard(project, key) {
   })
   console.log(projectImage)
   return (
-    <div key={key} style={{ backgroundImage: `url(${[projectImage[0]]})`}} className='w-1/3 bg-cover bg-center p-10 bg-gradient-to-t from-yellow-500 to-yellow-400 hover:to-yellow-500 rounded-md flex flex-wrap text-black translate-y-0 hover:-translate-y-4 ease-in-out duration-500'>
-      <p>{project.title}</p>
-      <p>{project.link}</p>
+    <div key={key} style={{ backgroundImage: `url(${[projectImage[0]]})`}} className='w-1/3 bg-cover bg-center rounded-md flex flex-wrap text-black translate-y-0 hover:-translate-y-4 ease-in-out duration-500'>
+      <div className='w-full h-full bg-gradient-to-t from-black/[.5] to-black/[.3]'>
+        <p>{project.title}</p>
+        <p>{project.link}</p>
+      </div>
     </div>
   )
 }
