302c378f356695e6ee509b2ae20846770db59d27
mpenajoia <penajoia.marco@gmail.com>
2022-04-04 14:14:54 -0700

    Hover opacity tweak on titles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ function ProjectCard(project, key) {
     <div key={key} style={{ backgroundImage: `url(${[projectImage[0]]})`}} className='w-1/3 bg-cover bg-center rounded-md flex flex-wrap hover:scale-105 ease-in-out duration-300'>
 
       <div className='flex flex-col justify-center items-center w-full pt-10 pb-40 hover:bg-gradient-to-t from-black/[.85] to-black/[.25] overflow-hidden group'>
-          <div className='-translate-y-80 group-hover:translate-y-12 duration-300 ease-in-out flex flex-col justify-center items-center gap-6'>
+          <div className='opacity-0 group-hover:opacity-100 duration-300 ease-in-out flex flex-col justify-center items-center gap-6'>
             <a href={project.link} target="_blank" rel="noreferrer" >
               <h4 className='text-4xl text-center font-bold duration-300 ease-in-out hover:text-yellow-500 drop-shadow'>{project.title}</h4>
             </a>
@@ -27,7 +27,7 @@ function ProjectCard(project, key) {
               <Icon icon={icons.github} size={32} className='duration-300 ease-in-out hover:text-yellow-500 drop-shadow'/>
             </a>
           </div>
-          <div className='flex flex-col items-center translate-y-96 group-hover:translate-y-16 duration-300 ease-in-out'>
+          <div className='flex flex-col items-center translate-y-96 group-hover:translate-y-16 opacity-0 group-hover:opacity-100 duration-300 ease-in-out'>
             <div className='flex flex-wrap justify-evenly items-end text-yellow-500 font-bold w-3/4 text-center'>
               {techStack}
             </div>
