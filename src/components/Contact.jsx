683b729a71b35302d65f0c5b5e11b14b6365ddc8
mpenajoia <penajoia.marco@gmail.com>
2022-04-05 09:27:18 -0700

    contact form mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,21 +12,21 @@ function Contact() {
   const sendEmail = (e) => {
     e.preventDefault()
     emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_USER_ID)
-    .then((result) => {
-      setMessageSent(true)
-      setShowStatus('block')
-    }, (error) => {
-      setMessageSent(false)
-      setShowStatus('block')
-      console.log(error.text);
+      .then((result) => {
+        setMessageSent(true)
+        setShowStatus('block')
+      }, (error) => {
+        setMessageSent(false)
+        setShowStatus('block')
+        console.log(error.text);
     });
     e.target.reset()
   }
 
   return (
     <div className='flex justify-center my-10 target:pt-[60px]' id="contact">
-        <div className='flex flex-col w-2/5'>
-            <h2 className='text-6xl mb-10 font-bold text-center'>Get in touch</h2>
+        <div className='flex flex-col w-5/6 md:w-2/5'>
+            <h2 className='text-5xl md:text-6xl mb-10 font-bold text-center'>Get in touch</h2>
             <form className='flex flex-col gap-y-3 ' onSubmit={sendEmail} ref={form} >
                 <input required type="text" name="name" placeholder='Your Name *' className={inputClasses}/>
                 <input required type="email" name="email" placeholder='Your email *' className={inputClasses} />
