6a5f26e3049ecc72bf9b0bc81c43c0174c4ffefc
mpenajoia <penajoia.marco@gmail.com>
2022-04-04 20:45:38 -0700

    hero margin

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='w-full flex justify-center items-center bg-black py-16 mt-[60px] h-96'>
-        <h1 className='text-5xl font-black '>
+    <div className='w-full flex justify-center items-center bg-black py-16 mt-[60px]'>
+        <h1 className='text-5xl font-black my-36 '>
             My name is Marco Silva &
             <br/>
             I'm a <span className='text-yellow-500'>Front End Developer</span>
