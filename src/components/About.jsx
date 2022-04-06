54cbbc089deef3878b5e2c387e24b113df0e3442
mpenajoia <penajoia.marco@gmail.com>
2022-04-06 10:48:19 -0700

    preliminary complete mobile responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,11 @@ function About() {
     const [mobileDetails, setMobileDetails] = useState();
     const aboutNavClick = (e) => {
         setAboutDetails(e.target.value)
-        setMobileDetails(e.target.value)
+        if(aboutDetails === mobileDetails){
+            setMobileDetails()
+        }else{
+            setMobileDetails(e.target.value)
+        }
     }
 
     const aboutNavMap = about.skills.map((skill, key) => {
@@ -24,7 +28,7 @@ function About() {
         return (
             <li key={key} >
                 <div className=' group w-full flex flex-col justify-center' onClick={aboutNavClick}>                    
-                    <button className={`flex flex-col items-center md:items-start text-xl md:text-lg font-bold duration-300 ease-in-out focus:text-zinc-800 md:focus:text-yellow-500 hover:text-yellow-500 ${(aboutDetails === skill.title) ? 'md:text-yellow-500' : 'text-white'} bg-black md:bg-transparent py-5 md:py-0 focus:bg-yellow-500 md:focus:bg-transparent`} value={skill.title}>{skill.title}
+                    <button className={`flex flex-col items-center md:items-start text-xl md:text-lg font-bold duration-300 ease-in-out md:focus:text-yellow-500 md:hover:text-yellow-500 ${(aboutDetails === skill.title) ? 'md:text-yellow-500' : 'text-white'} bg-black md:bg-transparent py-5 md:py-0 ${(mobileDetails === skill.title) ? 'focus:bg-yellow-500 focus:text-zinc-800' : ''} md:focus:bg-transparent`} value={skill.title}>{skill.title}
                         
                         <div className={` hidden md:block border-t-2 duration-300 ease-in-out group-hover:w-full group-hover:border-yellow-500 ${(aboutDetails === skill.title) ? 'w-full border-yellow-500' : 'w-0'}`}></div>
                     </button>
@@ -43,7 +47,7 @@ function About() {
         <div className='flex flex-col w-full md:w-2/3 gap-5'>
             <h2 className='text-5xl md:text-6xl font-bold text-center mb-10'>About Me</h2>
             <div className='flex flex-col md:flex-row gap-5'>
-                <div className=' md:w-2/5 self-center flex flex-col gap-5 items-center'>
+                <div className=' md:w-2/5 self-center flex flex-col gap-8 md:gap-5 items-center'>
                     <img src={Photo} alt="Me" />
                     <div className='flex justify-center items-center relative w-3/5 '>
                         <a href="https://penajoia.com/resume" className=' relative flex gap-2 justify-center bg-zinc-700 hover:bg-yellow-500 duration-300 ease-in-out rounded py-3 px-4 font-bold' target="_blank" rel="noreferrer">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,15 +16,15 @@ function ProjectCard(project, key) {
     )
   })
   return (
-    <div key={key} style={{ backgroundImage: `url(${[projectImage[0]]})`}} className='w-full md:w-1/3 bg-cover bg-center rounded-md flex flex-wrap md:hover:scale-105 ease-in-out duration-300'>
+    <div key={key} style={{ backgroundImage: `url(${[projectImage[0]]})`}} className='w-full md:w-1/3 bg-cover bg-center rounded-md flex flex-wrap hover:scale-105 ease-in-out duration-300'>
 
-      <div className='flex flex-col justify-center items-center w-full pt-10 pb-40 hover:bg-gradient-to-t from-black/[.85] to-black/[.35] md:overflow-hidden group'>
+      <div className='flex flex-col justify-center items-center w-full h-96  duration-300 hover:bg-gradient-to-t from-black/[.85] to-black/[.35] md:overflow-hidden group'>
           
-          <div className='md:opacity-0 md:group-hover:opacity-100 duration-500 ease-in-out flex flex-col justify-center items-center gap-6'>
+          <div className='opacity-0 group-hover:opacity-100 duration-500 ease-in-out flex flex-col justify-center items-center gap-6 md:mb-0 mb-6'>
             
             <a href={project.link} target="_blank" rel="noreferrer" >
               
-              <h4 className='text-4xl text-center font-bold duration-300 ease-in-out hover:text-yellow-500 drop-shadow'>{project.title}</h4>
+              <h4 className='text-4xl text-center font-bold duration-300 ease-in-out md:hover:text-yellow-500 drop-shadow'>{project.title}</h4>
             </a>
             
             <a href={project.github} target="_blank" rel="noreferrer" >
@@ -33,12 +33,12 @@ function ProjectCard(project, key) {
             </a>
           </div>
           
-          <div className='flex flex-col items-center md:translate-y-96 md:group-hover:translate-y-16 md:opacity-0 md:group-hover:opacity-100 duration-300 ease-in-out'>
+          <div className='flex flex-col items-center md:translate-y-96 md:group-hover:translate-y-16 opacity-0 group-hover:opacity-100 duration-300 ease-in-out'>
             
             <div className='flex flex-wrap justify-evenly items-end text-yellow-500 font-bold w-3/4 text-center'>
               {techStack}
             </div>
-            <p className='my-2 px-6 text-white'>{project.desc}</p>
+            <p className='my-2 px-6 text-white text-lg md:text-base leading-2 md:leading-none'>{project.desc}</p>
           </div>
       </div>
     </div>
