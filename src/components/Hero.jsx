c799de2b9bb19e7fc7278e8c7a9f0febf455bc21
mpenajoia <penajoia.marco@gmail.com>
2022-04-12 10:15:35 -0700

    hero project images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ObjectContext } from '../context/ObjectContext';
+import { Cryptofomo, ResumeBuilder, Netfolio } from '../img/index';
 
 function Hero() {
+  const projectImagesList = [Netfolio, Cryptofomo, ResumeBuilder]
+  const { info } = useContext(ObjectContext);
+  const projects = info.projects
+  const heroProjects = projects.map((item, key) => {
+    const heroThumb = projectImagesList.filter(proj => {
+      return proj.includes(item.image.toLowerCase())
+    })
+    return(
+      <div key={key} className='w-[300px] rounded-lg flex flex-col text-center'>
+        <img alt={item.title} src={heroThumb} />
+            <p className='bg-zinc-800 py-5 rounded-b-lg font-bold text-yellow-500'>{item.title}</p>
+      </div>
+    )
+  })
+
   return (
-    <div className='w-full flex justify-center items-center bg-black py-16 md:mt-[60px]'>
-        <h1 className=' text-4xl px-10 md:text-5xl font-black md:my-36 my-16 '>
+    <div className='w-5/6 flex justify-center items-center bg-black py-16 md:mt-[60px] relative overflow-hidden'>
+        <h1 className='ml-5 text-4xl md:text-5xl font-black md:my-36 my-16 relative z-10 drop-shadow-lg'>
             My name is&nbsp;
             <br className='md:hidden' />
             Marco Silva &
@@ -14,6 +31,9 @@ function Hero() {
             <br className='md:hidden' />
             Los Angeles.
         </h1>
+        <div className='hidden absolute lg:flex lg:opacity-60 xl:opacity-100 gap-4 right-0 pointer-events-none skew'>
+          {heroProjects}
+        </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { About, Navbar, Hero, Projects, Footer, Contact } from './index'
 
 function Home() {
