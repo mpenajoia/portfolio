0176f085f66eef3f022a034432367e34332ab519
mpenajoia <penajoia.marco@gmail.com>
2022-04-13 15:31:12 -0700

    updated cF

--- src/img/index.jsx
+++ src/img/index.jsx
@@ -1,3 +1,4 @@
 export { default as Netfolio } from './netfolio.jpg';
-export { default as Cryptofomo } from './cryptofomo.jpg';
+export { default as Cryptofomo } from './cryptofomo2.jpg';
+export { default as Cryptofomo2 } from './cryptofomo.jpg';
 export { default as ResumeBuilder } from './resumebuilder.jpg';
